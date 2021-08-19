7332e0ae8251bca437607370ded9951fa796d9b0
ds_95 <ds_95@192.168.0.101>
2021-08-19 11:52:39 -0300

    Testing the commit.

--- AbstractFactory/src/app/app.java
+++ AbstractFactory/src/app/app.java
@@ -7,6 +7,7 @@ public class app {
 
 	
 	public static void main(String[] args) {
+		System.out.println("Welcome to the app !");
 		FurnitureAbstractFactory modernfactory = new ModernStyleFactory("'Future'");
 		FurnitureAbstractFactory rusticFactory = new RusticStyleFactory("'Antique'");
 		
